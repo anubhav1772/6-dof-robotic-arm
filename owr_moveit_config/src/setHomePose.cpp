fc0cd7621ec21d7fbe9413e910fa2ab0d3fe74ec
anubhav1772 <anubhavsingh55182@gmail.com>
2023-05-30 01:57:17 +0530

    added gripper's setNamedTarget

--- owr_moveit_config/src/setHomePose.cpp
+++ owr_moveit_config/src/setHomePose.cpp
@@ -10,6 +10,7 @@
 
 typedef boost::shared_ptr<moveit::planning_interface::MoveGroupInterface> MoveGroupPtr;
 const std::string ARM_PLANNING_GROUP = "arm_manipulator";
+//const std::string GRIPPER_PLANNING_GROUP = "gripper";
 
 int main(int argc, char** argv)
 {
@@ -21,8 +22,10 @@ int main(int argc, char** argv)
 
   MoveGroupPtr arm_move_group = MoveGroupPtr(new moveit::planning_interface::MoveGroupInterface(ARM_PLANNING_GROUP));
   // arm_move_group->setPoseReferenceFrame(frame_id_);
+  //MoveGroupPtr gripper_move_group = MoveGroupPtr(new moveit::planning_interface::MoveGroupInterface(GRIPPER_PLANNING_GROUP));
 
   arm_move_group->setNamedTarget("HOME");
+  //gripper_move_group->setNamedTarget("CLOSED");
 
   moveit::planning_interface::MoveGroupInterface::Plan my_plan;
   bool success = (arm_move_group->plan(my_plan) == moveit::planning_interface::MoveItErrorCode::SUCCESS);
@@ -30,6 +33,11 @@ int main(int argc, char** argv)
     return false;
 
   arm_move_group->move();
+
+  //ros::Duration(2.0).sleep();
+
+  //gripper_move_group->move();
+
   spinner.stop();
 
   ros::shutdown();
